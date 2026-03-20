40d35066a57fd4fe88de454387fc13f1bd9c10ce
khushianandyadav <khushiy1904@gmail.com>
2026-03-20 15:17:07 +0530

    Create main.py

---
+++ VARS_interface/main.py
@@ -0,0 +1,14 @@
+print("✅ main.py is running")
+
+
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from interface.video_window import VideoWindow
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    player = VideoWindow()
+    player.showMaximized()
+    sys.exit(app.exec_())
